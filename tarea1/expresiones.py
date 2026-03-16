b8ec8a4f5903afca054113680b467ba41c03a2c7
fridadhz <111761698+fridadhz@users.noreply.github.com>
2026-03-16 15:56:30 -0600

    Update expresiones.py

--- tarea1/expresiones.py
+++ tarea1/expresiones.py
@@ -25,3 +25,63 @@ pruebas_nmatch ["CODIGO_DE_EJERCICIO"] = ["#HolaMundo","#UNAM ONOS","#Covid 19",
 
 #NOTA: el CODIGO_DE_EJERCICIO es la cadena que se encuentra entre [ ] en el enunciado del ejercicio.
 #Por ejemplo: [e03] Σ = {a, b,..., z}. Cadenas que no contienen tres consonantes consecutivas.
+
+#e02
+ejercicios["e02"] = re.compile(r'^([a-z0-9][^aeiou])([a-z0-9][^aeiou])*(?:[a-z0-9])?$')
+
+pruebas_match["e02"] = [
+"a1b2",
+"b3c4",
+"z9",
+"m2n4",
+"x5y"
+]
+
+pruebas_nmatch["e02"] = [
+"ba",
+"a2e",
+"c4i",
+"d8o",
+"g2u"
+]
+
+
+#e12
+ejercicios["e12"] = re.compile(r'^([a-z]+|[0-9]+)$')
+
+pruebas_match["e12"] = [
+"abc",
+"xyz",
+"hola",
+"123",
+"98765"
+]
+
+pruebas_nmatch["e12"] = [
+"a1",
+"b2c",
+"hola3",
+"4test",
+"x9y"
+]
+
+
+#e24
+ejercicios["e24"] = re.compile(r'^([a-z0-9]*[02468]){1,2}[a-z0-9]*$')
+
+pruebas_match["e24"] = [
+"a2",
+"b4c",
+"z8x6",
+"hola2",
+"test4a"
+]
+
+pruebas_nmatch["e24"] = [
+"abc",
+"135",
+"7test",
+"xyz",
+"bdfg"
+]
+
